545ff8e25c3fcc24389ee7b804808429544bf1a6
indungu <in.wainaina93@gmail.com>
2017-10-19 16:47:03 +0300

    [ADD] User create capability

--- app/users.py
+++ app/users.py
@@ -1,5 +1,24 @@
 """This module manages the users"""
+USERS = {
+    "indungu": {"email": "in@yummy.io", "username": "indungu", "password": "pass2017"},
+    "cbreezy": {"email": "cb@yummy.io", "username": "cbreezy", "password": "questions"}
+}
 
 class User(object):
     """This class manages the app users"""
-    pass
+
+    def __init__(self, email=None, username=None, password=None):
+        """Creates and instance of a user"""
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def add_user(self, email, username, password):
+        """This method adds a new user to the app"""
+        current_users = USERS.keys()
+        if username not in current_users:
+            new_user = {"email": email, "username": username, "password": password}
+            USERS[username] = new_user
+            return USERS
+        elif username in USERS.keys():
+            return "Sorry, that username is already taken, choose a different one"    
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,10 +3,20 @@
 import unittest
 
 from app.users import User
+from app.users import USERS
 
-class UserManagemertTestCase(unittest.TestCase):
+class UserManagementTestCase(unittest.TestCase):
     """This class tests the users.py"""
+
+    def setUp(self):
+        """Set up test"""
+        self.new_user = User()
+        self.current_users = USERS
+        self.user_email = "fn@yummy.io"
+        self.username = "fena"
+        self.password = "fenamenal"
 
     def test_user_creation(self):
         """test user is added successfully"""
-        new_user = User()
+        test_user = self.new_user.add_user(self.user_email, self.username, self.password)
+        self.assertTrue(self.username in test_user.keys())
