771c4fc698ef97d4d79a35be5ad47bc494794c01
indungu <in.wainaina93@gmail.com>
2017-10-25 16:04:24 +0300

    [FEAT] CRUD for Recipies, [ADD] Unit tests

--- app/recipes.py
+++ app/recipes.py
@@ -10,7 +10,7 @@ class Categories(object):
         """Initialize objects of this class with the following"""
         self.name = ''
         self.description = ''
-        self.recipes = []
+        self.recipes = {}
 
     def add_category(self, name, description):
         """Add to categories list"""
@@ -23,7 +23,7 @@ class Categories(object):
             "description": self.description,
             "recipes": self.recipes
         }
-        return CATEGORIES[self.name]
+        return CATEGORIES[name]
 
     def get_category(self, name):
         """Returns the named category or error message"""
@@ -44,3 +44,45 @@ class Categories(object):
             CATEGORIES.pop(name)
             return "Removed successfully."
         return "Category does not exist."
+
+class Recipes(object):
+    """
+    This is subclass of the Categories class
+    This class contains the CRUD features for recipes
+    """
+
+    def __init__(self):
+        """Default constructor"""
+
+    def add_recipe(self, category, recipe):
+        """Add a new recipe only to existing categories"""
+        if category in CATEGORIES:
+            CATEGORIES[category]['recipes'][recipe['name']] = recipe
+            return "Recipe added successfully."
+        return "Category does not exist."
+
+    def get_recipe(self, category, name):
+        """Get the recipe from the spefied category"""
+        if category not in CATEGORIES:
+            return "Recipe does not exist"
+        if name in CATEGORIES[category]['recipes']:
+            recipe = CATEGORIES[category]['recipes'][name]
+            return recipe
+
+    def set_recipe(self, category, name, content):
+        """Get the recipe from the spefied category"""
+        if category not in CATEGORIES:
+            return "Recipe does not exist"
+        if name in CATEGORIES[category]['recipes']:
+            CATEGORIES[category]['recipes'][name].update(content)
+            temp_recipe = CATEGORIES[category]['recipes'].pop(name)
+            CATEGORIES[category]['recipes'][temp_recipe['name']] = temp_recipe
+            return CATEGORIES[category]['recipes']
+        return "Recipe does not exist"
+
+    def delete_recipe(self, category, name):
+        """Removes the named recipe in the category provided"""
+        if category in CATEGORIES and name in CATEGORIES[category]['recipes']:
+            CATEGORIES[category]['recipes'].pop(name)
+            return "Recipe deleted successfully!"
+        return "Recipe does not exist!"
