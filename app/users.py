7d8b930e04586707d55face42b8c02bfba9c7401
indungu <in.wainaina93@gmail.com>
2017-11-04 16:57:11 +0300

    [REFACTOR] User managment to use OOP logic, refactor tests

--- app/users.py
+++ app/users.py
@@ -1,30 +1,27 @@
 """This module manages the users"""
+from werkzeug.security import generate_password_hash
 USERS = {}
 
 class User(object):
     """This class manages the app users"""
 
-    def __init__(self, email=None, username=None, password=None):
+    def __init__(self, email, username, password):
         """Creates and instance of a user"""
         self.email = email
         self.username = username
-        self.password = password
+        self.password = generate_password_hash(password)
 
-    def add_user(self, email, username, password):
-        """This method adds a new user to the app"""
-        if email not in USERS:
-            USERS[email] = {"email": email, "username": username, "password": password}
+    def add_user(self):
+        """This method adds a new user to the USERS dictionary"""
+        if self.email not in USERS:
+            USERS[self.email] = self
             return "User added successfully."
         return "Sorry, that email is already registered."
 
-    def get_user(self, email, password):
+    def get_user(self):
         """This method gets an existing user from the users's list"""
-        # Get an existing user
-        # First check whether user account exists
-        if email in USERS:
-            current_user = USERS[email]
-            # confirm that correct password was entered
-            if current_user["password"] == password:
-                return current_user
-            return "Password error!"
-        return "User not found!"
+        return (self.email, self.password)
+
+    def delete_user(self):
+        """This method deletes a user"""
+        USERS.pop(self.email)
